c53f3b0cf8a524c8ec894376b4a6c8faaf6b4c08
Shayan Mouktar <119674649+Shayan105@users.noreply.github.com>
2026-01-06 20:15:37 +0100

    Fix typos

--- farihy-website/src/main.tsx
+++ farihy-website/src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import RoomCard from "./components/RoomCard";
 import VerticalPicture from "./components/VerticalPicture";
-import MassageGallery from "./farihy_components/MassageImageGalery";
-import RestaurantGallery from "./farihy_components/RestaurantImageGalery";
+import MassageGallery from "./farihy_components/MassageImageGallery";
+import RestaurantGallery from "./farihy_components/RestaurantImageGallery";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
