58af43c432726f74704e29d765ab1ad2a357fddc
Shayan Mouktar <119674649+Shayan105@users.noreply.github.com>
2026-01-06 21:28:30 +0100

    Center restaurant pics

--- farihy-website/src/farihy_components/RestaurantImageGallery.tsx
+++ farihy-website/src/farihy_components/RestaurantImageGallery.tsx
@@ -1,50 +1,58 @@
-import React from 'react';
-import ImageGallery from '../components/ImageGallery';
+import React from "react";
+import ImageGallery from "../components/ImageGallery";
 
 /**
- * Restaurant serves as the data provider for the 
+ * Restaurant serves as the data provider for the
  * generic ImageGallery component.
  */
 const RestaurantGallery: React.FC = () => {
-    const basePath = "/src/assets/pictures/restaurant/";
+  const basePath = "/src/assets/pictures/restaurant/";
   const massageContent = {
     title: "Le restaurant",
-    subtitle: "Installez-vous sur la terrasse, la plage ou dans le jardin, laissez-vous porter par la vue sur le lac Itasy et savourez l’instant au bord de la piscine.",
+    subtitle:
+      "Installez-vous sur la terrasse, la plage ou dans le jardin, laissez-vous porter par la vue sur le lac Itasy et savourez l’instant au bord de la piscine.",
     images: [
-      { 
-        path: basePath+"restaurant-1.jpg", 
-        alt: "" 
+      {
+        path: basePath + "restaurant-1.jpg",
+        focusPosition: "right",
       },
-            { 
-        path: basePath+"restaurant-2.jpg", 
-        alt: "" 
-      },      { 
-        path: basePath+"restaurant-3.jpg", 
-        alt: "" 
-      },      { 
-        path: basePath+"restaurant-4.jpg", 
-        alt: "" 
-      },      { 
-        path: basePath+"restaurant-5.jpg", 
-        alt: "" 
-      },      { 
-        path: basePath+"restaurant-6.jpg", 
-        alt: "" 
-      },      { 
-        path: basePath+"restaurant-7.jpg", 
-        focusPosition:"30%",
-        alt: "" 
+      {
+        path: basePath + "restaurant-2.jpg",
+        focusPosition: "30%",
       },
-
-    ]
+      {
+        path: basePath + "restaurant-3.jpg",
+        focusPosition: "10% ",
+      },
+      {
+        path: basePath + "restaurant-4.jpg",
+        focusPosition: "left",
+      },
+      {
+        path: basePath + "restaurant-4.jpg",
+        focusPosition: "right",
+      },
+      {
+        path: basePath + "restaurant-5.jpg",
+        focusPosition: "10%",
+      },
+      {
+        path: basePath + "restaurant-6.jpg",
+        focusPosition: "left",
+      },
+      {
+        path: basePath + "restaurant-7.jpg",
+        focusPosition: "30%",
+        
+      },
+    ],
   };
 
   return (
-    <ImageGallery 
+    <ImageGallery
       title={massageContent.title}
       subtitle={massageContent.subtitle}
       images={massageContent.images}
-
     />
   );
 };
